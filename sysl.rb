852e4fee0b60385d2e3bea1ac458ebf7efdc7ec0
Ashwin Sajiv <sajivashwin@gmail.com>
2020-02-24 14:00:46 +1100

    \

--- sysl.rb
+++ sysl.rb
@@ -1,5 +1,5 @@
 class Sysl < Formula
-  desc "Sysl (pronounced "sizzle") is a system specification language"
+  desc "Sysl (pronounced \"sizzle\") is a system specification language"
   homepage "https://sysl.io"
   url "https://github.com/anz-bank/sysl/archive/v0.6.3.tar.gz"
   sha256 "a0d150a7f4051b1d4739226bd2e859d48e430a66c5591a478ab6697284ed56e5"
